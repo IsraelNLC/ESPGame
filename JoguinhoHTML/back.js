73d36260ecb787470e8782cbfc36a41182f97444
Israel <israel.carvalho@sou.inteli.edu.br>
2022-11-27 23:40:32 -0300

    Teste 1

---
+++ JoguinhoHTML/back.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const hostname = '127.0.0.1';
+const port = 3061;
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.static("../JoguinhoHTML/"));
+app.use(express.json());
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
